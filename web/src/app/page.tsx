aeef2d43a821ca217eaafbcb4cfdaf76fc5fa224
HEKPYTO <tsunyanapat@gmail.com>
2025-04-04 19:54:58 +0700

    CLEANUP

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,15 +1,13 @@
 import ChessBoard from '@/components/chess/ChessBoard'
-import PgnViewer from '@/components/chess/PgnViewer'
 import { Separator } from "@/components/ui/separator"
 
 export default function Home() {
-  const samplePgn = "1. e4 e5 2. Nf3 Nc6 3. Bb5 a6";
 
   return (
     <div className="container mx-auto py-8 px-4">
       <h1 className="text-3xl font-bold text-center mb-8">Chess App</h1>
       
-        <ChessBoard playingAs='b' />
+        <ChessBoard playingAs='w' />
       
       <Separator className="my-8" />
       
