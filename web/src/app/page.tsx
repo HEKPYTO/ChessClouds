06dddef0e0db2b526643b0f5a1de54dc6886f428
HEKPYTO <tsunyanapat@gmail.com>
2025-04-05 20:58:09 +0700

    RE WEB WORKFLOW

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Game() {
 
   return (
     <div className="container mx-auto py-8 px-4">
-      <Pane playingAs="w"></Pane>
+      <Pane playingAs="w" />
       
       <div className="text-center text-gray-500 text-sm py-10">
         Built by ChessCloud Group
