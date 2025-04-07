46f7e78818a8ccf177cdc7c26e525df84eb80b56
Mond45 <mondc45@gmail.com>
2025-04-07 23:55:21 +0700

    restrict each player to 1 socket connection
    - keep track of connected players in game state and act accordingly
    during authentication
    - change socket read logic to only read incoming Message::Text
    - update tests
    - use select instead of join to detect socket closure

--- ws_server/src/bin/test.rs
+++ ws_server/src/bin/test.rs
@@ -19,7 +19,7 @@ const BLACK_ID: &'static str = "black";
 
 async fn send_msg(
     writer: &mut SplitSink<WebSocketStream<MaybeTlsStream<TcpStream>>, Message>,
-    msg: &ClientMessage,
+    msg: ClientMessage,
 ) -> anyhow::Result<()> {
     let msg = serde_json::to_string(&msg)?;
     writer.send(Message::Text(Utf8Bytes::from(&msg))).await?;
@@ -29,96 +29,166 @@ async fn send_msg(
 
 fn spawn_reader(mut reader: SplitStream<WebSocketStream<MaybeTlsStream<TcpStream>>>) {
     tokio::spawn(async move {
-        while let Some(Ok(msg)) = reader.next().await {
-            if let Message::Text(text) = msg {
-                println!("Received: {}", text.to_string());
-            }
+        while let Some(Ok(Message::Text(text))) = reader.next().await {
+            println!("Received: {}", text.to_string());
         }
     });
 }
 
+async fn connect_socket(
+    url: &str,
+) -> anyhow::Result<SplitSink<WebSocketStream<MaybeTlsStream<TcpStream>>, Message>> {
+    let (socket, _) = connect_async(url).await?;
+    let (writer, reader) = socket.split();
+
+    spawn_reader(reader);
+
+    Ok(writer)
+}
+
 async fn test_auth(url: &str) -> anyhow::Result<()> {
     {
-        // should fail
-        let (socket, _) = connect_async(url).await?;
-        let (mut writer, reader) = socket.split();
+        println!("should fail: not Message::Text");
+        let mut writer = connect_socket(url).await?;
 
-        spawn_reader(reader);
+        writer
+            .send(Message::Ping(Bytes::from_static(b"abc")))
+            .await?;
+
+        writer.close().await?;
+    }
+    {
+        println!("should fail: deserialization");
+        let mut writer = connect_socket(url).await?;
+
+        writer
+            .send(Message::Text(Utf8Bytes::from_static("abc")))
+            .await?;
+
+        writer.close().await?;
+    }
+    {
+        println!("should fail: not-existent game");
+        let mut writer = connect_socket(url).await?;
 
         send_msg(
             &mut writer,
-            &ClientMessage::Auth {
+            ClientMessage::Auth {
                 game_id: "abc".to_string(),
                 user_id: WHITE_ID.to_string(),
             },
         )
         .await?;
+
+        writer.close().await?;
     }
     {
-        // should fail
-        let (socket, _) = connect_async(url).await?;
-        let (mut writer, reader) = socket.split();
-
-        spawn_reader(reader);
+        println!("should fail: user not black or white");
+        let mut writer = connect_socket(url).await?;
 
         send_msg(
             &mut writer,
-            &ClientMessage::Auth {
+            ClientMessage::Auth {
                 game_id: GAME_ID.to_string(),
                 user_id: "brown".to_string(),
             },
         )
         .await?;
+
+        writer.close().await?;
     }
     {
-        // should fail
-        let (socket, _) = connect_async(url).await?;
-        let (mut writer, reader) = socket.split();
+        println!("should succeed: move, auth white");
+        let mut writer = connect_socket(url).await?;
 
-        spawn_reader(reader);
-
-        writer
-            .send(Message::Ping(Bytes::from_static(b"abc")))
-            .await?;
-        writer
-            .send(Message::Text(Utf8Bytes::from_static("abc")))
-            .await?;
+        send_msg(&mut writer, ClientMessage::Move("e4".to_string())).await?;
         send_msg(
             &mut writer,
-            &ClientMessage::Auth {
+            ClientMessage::Auth {
                 game_id: GAME_ID.to_string(),
-                user_id: "brown".to_string(),
+                user_id: WHITE_ID.to_string(),
             },
         )
         .await?;
+
+        writer.close().await?;
     }
     {
-        // should fail
-        let (socket, _) = connect_async(url).await?;
-        let (mut writer, reader) = socket.split();
+        println!("should succeed: auth black");
+        let mut writer = connect_socket(url).await?;
 
-        spawn_reader(reader);
+        send_msg(
+            &mut writer,
+            ClientMessage::Auth {
+                game_id: GAME_ID.to_string(),
+                user_id: BLACK_ID.to_string(),
+            },
+        )
+        .await?;
 
-        send_msg(&mut writer, &ClientMessage::Move("e4".to_string())).await?;
+        writer.close().await?;
     }
+    {
+        println!("should fail: white already connected");
+        let mut writer1 = connect_socket(url).await?;
+        let mut writer2 = connect_socket(url).await?;
 
+        send_msg(
+            &mut writer1,
+            ClientMessage::Auth {
+                game_id: GAME_ID.to_string(),
+                user_id: WHITE_ID.to_string(),
+            },
+        )
+        .await?;
+        send_msg(
+            &mut writer2,
+            ClientMessage::Auth {
+                game_id: GAME_ID.to_string(),
+                user_id: WHITE_ID.to_string(),
+            },
+        )
+        .await?;
+
+        writer1.close().await?;
+        writer2.close().await?;
+    }
+    {
+        println!("should fail: black already connected");
+        let mut writer1 = connect_socket(url).await?;
+        let mut writer2 = connect_socket(url).await?;
+
+        send_msg(
+            &mut writer1,
+            ClientMessage::Auth {
+                game_id: GAME_ID.to_string(),
+                user_id: BLACK_ID.to_string(),
+            },
+        )
+        .await?;
+        send_msg(
+            &mut writer2,
+            ClientMessage::Auth {
+                game_id: GAME_ID.to_string(),
+                user_id: BLACK_ID.to_string(),
+            },
+        )
+        .await?;
+
+        writer1.close().await?;
+        writer2.close().await?;
+    }
     Ok(())
 }
 
 async fn test_moves(url: &str) -> anyhow::Result<()> {
     {
-        let (socket_white, _) = connect_async(url).await?;
-        let (mut writer_white, reader_white) = socket_white.split();
-
-        let (socket_black, _) = connect_async(url).await?;
-        let (mut writer_black, reader_black) = socket_black.split();
-
-        spawn_reader(reader_white);
-        spawn_reader(reader_black);
+        let mut writer_black = connect_socket(url).await?;
+        let mut writer_white = connect_socket(url).await?;
 
         send_msg(
             &mut writer_white,
-            &ClientMessage::Auth {
+            ClientMessage::Auth {
                 game_id: GAME_ID.to_string(),
                 user_id: WHITE_ID.to_string(),
             },
@@ -126,27 +196,30 @@ async fn test_moves(url: &str) -> anyhow::Result<()> {
         .await?;
         send_msg(
             &mut writer_black,
-            &ClientMessage::Auth {
+            ClientMessage::Auth {
                 game_id: GAME_ID.to_string(),
                 user_id: BLACK_ID.to_string(),
             },
         )
         .await?;
 
-        // should fail: deserialization
+        println!("should fail: deserialization");
         writer_black
             .send(Message::Text(Utf8Bytes::from_static("abc")))
             .await?;
-        // should fail: invalid turn
-        send_msg(&mut writer_black, &ClientMessage::Move("e5".to_string())).await?;
-        // should fail: invalid move
-        send_msg(&mut writer_white, &ClientMessage::Move("abc".to_string())).await?;
-        // should fail: invalid move
-        send_msg(&mut writer_white, &ClientMessage::Move("e5".to_string())).await?;
-        // should succeed
-        send_msg(&mut writer_white, &ClientMessage::Move("e4".to_string())).await?;
-        // should fail: invalid turn
-        send_msg(&mut writer_white, &ClientMessage::Move("d4".to_string())).await?;
+        println!("should fail: invalid turn");
+        send_msg(&mut writer_black, ClientMessage::Move("e5".to_string())).await?;
+        println!("should fail: invalid move");
+        send_msg(&mut writer_white, ClientMessage::Move("abc".to_string())).await?;
+        println!("should fail: invalid move");
+        send_msg(&mut writer_white, ClientMessage::Move("e5".to_string())).await?;
+        println!("should succeed");
+        send_msg(&mut writer_white, ClientMessage::Move("d4".to_string())).await?;
+        println!("should fail: invalid turn");
+        send_msg(&mut writer_white, ClientMessage::Move("e4".to_string())).await?;
+
+        writer_white.close().await?;
+        writer_black.close().await?;
     }
 
     Ok(())
