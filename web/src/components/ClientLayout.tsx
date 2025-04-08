474cddfa249269dde577bba6c979c476249f5d92
HEKPYTO <tsunyanapat@gmail.com>
2025-04-08 15:35:57 +0700

    ADD BUTTONS: HOMEPAGE AND THEME

---
+++ web/src/components/ClientLayout.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+import Navbar from '@/components/NavBar';
+import Footer from '@/components/Footer';
+
+export default function ClientLayout({ 
+  children 
+}: { 
+  children: React.ReactNode 
+}) {
+  const pathname = usePathname();
+  const isAuthPage = pathname === '/signin' || pathname === '/signup';
+
+  return (
+    <>
+      {!isAuthPage && <Navbar />}
+      <main className={`flex-grow ${!isAuthPage ? 'pt-16' : ''} relative`}>
+        {children}
+      </main>
+      {!isAuthPage && <Footer />}
+    </>
+  );
+}
