680b52d4d2181d575b5c9beb0a828e36aa76f4a1
Mond45 <mondc45@gmail.com>
2025-04-17 01:24:22 +0700

    matchmaking: implement database connection

--- matchmaking/src/main.rs
+++ matchmaking/src/main.rs
@@ -3,7 +3,13 @@ use std::{
     sync::{Arc, RwLock},
 };
 
-use axum::{routing::post, Json, Router};
+use axum::{http::StatusCode, routing::post, Json, Router};
+use diesel_async::{
+    pooled_connection::{deadpool::Pool, AsyncDieselConnectionManager},
+    RunQueryDsl,
+};
+use dotenvy::dotenv;
+use matchmaking::{models::Activegame, schema::activegames};
 use serde::{Deserialize, Serialize};
 use tokio::sync::{mpsc, oneshot};
 
@@ -31,10 +37,15 @@ pub struct MatchResponse {
 
 pub struct MatchingPlayer {
     pub user_id: String,
-    pub tx: oneshot::Sender<MatchResponse>,
+    pub tx: oneshot::Sender<Option<MatchResponse>>,
 }
 
 async fn matcher(mut rx: mpsc::Receiver<()>, player_queue: ConcurrentQueue<MatchingPlayer>) {
+    let config = AsyncDieselConnectionManager::<diesel_async::AsyncPgConnection>::new(
+        std::env::var("DATABASE_URL").expect("DATABASE_URL not present in .env"),
+    );
+    let pool = Pool::builder(config).build().unwrap();
+
     while let Some(_) = rx.recv().await {
         while player_queue.read().unwrap().len() >= 2 {
             let (player1, player2) = {
@@ -45,24 +56,51 @@ async fn matcher(mut rx: mpsc::Receiver<()>, player_queue: ConcurrentQueue<Match
                 )
             };
 
-            let game_id = uuid::Uuid::new_v4().to_string();
+            let game_id = uuid::Uuid::new_v4();
+
+            let game_info = Activegame {
+                gameid: game_id,
+                black: player2.user_id.clone(),
+                white: player1.user_id.clone(),
+                createdat: None,
+            };
+
+            let mut conn = match pool.get().await {
+                Ok(conn) => conn,
+                Err(e) => {
+                    tracing::error!("Error obtaining connection: {e}");
+                    player1.tx.send(None).unwrap();
+                    player2.tx.send(None).unwrap();
+                    continue;
+                }
+            };
+
+            if let Err(e) = diesel::insert_into(activegames::table)
+                .values(game_info)
+                .execute(&mut conn)
+                .await
+            {
+                tracing::error!("Insertion failed: {e}");
+                player1.tx.send(None).unwrap();
+                player2.tx.send(None).unwrap();
+                continue;
+            }
 
             tracing::info!("Matched {} and {}", player1.user_id, player2.user_id);
 
-            // TODO: Create an active game in DB
             player1
                 .tx
-                .send(MatchResponse {
-                    game_id: game_id.clone(),
+                .send(Some(MatchResponse {
+                    game_id: game_id.to_string(),
                     color: Color::White,
-                })
+                }))
                 .unwrap();
             player2
                 .tx
-                .send(MatchResponse {
-                    game_id: game_id.clone(),
+                .send(Some(MatchResponse {
+                    game_id: game_id.to_string(),
                     color: Color::Black,
-                })
+                }))
                 .unwrap();
         }
     }
@@ -72,7 +110,7 @@ pub async fn post_match(
     notify_tx: mpsc::Sender<()>,
     player_queue: ConcurrentQueue<MatchingPlayer>,
     Json(body): Json<MatchRequest>,
-) -> Json<MatchResponse> {
+) -> (StatusCode, Json<MatchResponse>) {
     let (res_tx, res_rx) = oneshot::channel();
 
     tracing::info!("User {} requests a match", body.user_id);
@@ -85,14 +123,22 @@ pub async fn post_match(
 
     notify_tx.send(()).await.unwrap();
 
-    let res = res_rx.await.unwrap();
-
-    Json(res)
+    match res_rx.await.unwrap() {
+        Some(res) => (StatusCode::OK, Json(res)),
+        None => (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            Json(MatchResponse {
+                game_id: "".to_string(),
+                color: Color::White,
+            }),
+        ),
+    }
 }
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
+    dotenv().ok();
 
     let (notify_tx, notify_rx) = mpsc::channel(MAX_CHANNEL_SIZE);
 
