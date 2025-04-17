96f047a0ff380b84d6fde2273afd370d8e954429
Mond45 <mondc45@gmail.com>
2025-04-17 21:12:36 +0700

    matchmaking: remove diesel, fix potential panics
    - remove diesel (to be replaced with sqlx)
    - fix potential panics that might occur when client closes connection
    while still waiting for response
    - auto clean up matching player records when client closes connection
    - ensure player already in queue can't enter matchmaking

--- matchmaking/src/main.rs
+++ matchmaking/src/main.rs
@@ -1,22 +1,35 @@
 use std::{
     collections::VecDeque,
-    sync::{Arc, RwLock},
+    sync::{Arc, Mutex},
 };
 
 use axum::{http::StatusCode, routing::post, Json, Router};
-use diesel_async::{
-    pooled_connection::{deadpool::Pool, AsyncDieselConnectionManager},
-    RunQueryDsl,
-};
 use dotenvy::dotenv;
-use matchmaking::{models::Activegame, schema::activegames};
 use serde::{Deserialize, Serialize};
 use tokio::sync::{mpsc, oneshot};
 
 const MAX_CHANNEL_SIZE: usize = 4096;
 const HOST: &'static str = "0.0.0.0:8001";
 
-type ConcurrentQueue<T> = Arc<RwLock<VecDeque<T>>>;
+type ConcurrentQueue<T> = Arc<Mutex<VecDeque<T>>>;
+
+struct AutoDrop {
+    user_id: String,
+    queue: ConcurrentQueue<MatchingPlayer>,
+}
+
+impl Drop for AutoDrop {
+    fn drop(&mut self) {
+        tracing::info!(
+            "User {} closed connection, removing matching player record",
+            self.user_id
+        );
+        self.queue
+            .lock()
+            .unwrap()
+            .retain(|x| x.user_id != self.user_id)
+    }
+}
 
 #[derive(Deserialize)]
 pub struct MatchRequest {
@@ -30,26 +43,33 @@ pub enum Color {
 }
 
 #[derive(Serialize, Debug)]
-pub struct MatchResponse {
-    pub game_id: String,
-    pub color: Color,
+pub enum MatchResponse {
+    Ok { game_id: String, color: Color },
+    Err(String),
+}
+
+impl MatchResponse {
+    pub fn is_err(&self) -> bool {
+        match self {
+            MatchResponse::Err(_) => true,
+            _ => false,
+        }
+    }
 }
 
 pub struct MatchingPlayer {
     pub user_id: String,
-    pub tx: oneshot::Sender<Option<MatchResponse>>,
+    pub tx: oneshot::Sender<MatchResponse>,
 }
 
 async fn matcher(mut rx: mpsc::Receiver<()>, player_queue: ConcurrentQueue<MatchingPlayer>) {
-    let config = AsyncDieselConnectionManager::<diesel_async::AsyncPgConnection>::new(
-        std::env::var("DATABASE_URL").expect("DATABASE_URL not present in .env"),
-    );
-    let pool = Pool::builder(config).build().unwrap();
-
     while let Some(_) = rx.recv().await {
-        while player_queue.read().unwrap().len() >= 2 {
+        loop {
             let (player1, player2) = {
-                let mut queue = player_queue.write().unwrap();
+                let mut queue = player_queue.lock().unwrap();
+                if queue.len() < 2 {
+                    break;
+                }
                 (
                     queue.pop_front().expect("Player should exist"),
                     queue.pop_front().expect("Player should exist"),
@@ -58,50 +78,22 @@ async fn matcher(mut rx: mpsc::Receiver<()>, player_queue: ConcurrentQueue<Match
 
             let game_id = uuid::Uuid::new_v4();
 
-            let game_info = Activegame {
-                gameid: game_id,
-                black: player2.user_id.clone(),
-                white: player1.user_id.clone(),
-                createdat: None,
-            };
-
-            let mut conn = match pool.get().await {
-                Ok(conn) => conn,
-                Err(e) => {
-                    tracing::error!("Error obtaining connection: {e}");
-                    player1.tx.send(None).unwrap();
-                    player2.tx.send(None).unwrap();
-                    continue;
-                }
-            };
-
-            if let Err(e) = diesel::insert_into(activegames::table)
-                .values(game_info)
-                .execute(&mut conn)
-                .await
-            {
-                tracing::error!("Insertion failed: {e}");
-                player1.tx.send(None).unwrap();
-                player2.tx.send(None).unwrap();
-                continue;
-            }
+            // TODO: insert into DB
 
             tracing::info!("Matched {} and {}", player1.user_id, player2.user_id);
 
-            player1
-                .tx
-                .send(Some(MatchResponse {
-                    game_id: game_id.to_string(),
-                    color: Color::White,
-                }))
-                .unwrap();
-            player2
-                .tx
-                .send(Some(MatchResponse {
-                    game_id: game_id.to_string(),
-                    color: Color::Black,
-                }))
-                .unwrap();
+            if let Err(_) = player1.tx.send(MatchResponse::Ok {
+                game_id: game_id.to_string(),
+                color: Color::White,
+            }) {
+                tracing::error!("Connection closed unexpectedly, proceeding anyway");
+            }
+            if let Err(_) = player2.tx.send(MatchResponse::Ok {
+                game_id: game_id.to_string(),
+                color: Color::Black,
+            }) {
+                tracing::error!("Connection closed unexpectedly, proceeding anyway");
+            }
         }
     }
 }
@@ -113,25 +105,38 @@ pub async fn post_match(
 ) -> (StatusCode, Json<MatchResponse>) {
     let (res_tx, res_rx) = oneshot::channel();
 
+    {
+        let mut queue = player_queue.lock().unwrap();
+
+        if queue.iter().any(|player| player.user_id == body.user_id) {
+            tracing::error!("User {} already in queue", body.user_id);
+            return (
+                StatusCode::BAD_REQUEST,
+                Json(MatchResponse::Err("Player already in queue".to_string())),
+            );
+        }
+
+        queue.push_back(MatchingPlayer {
+            user_id: body.user_id.clone(),
+            tx: res_tx,
+        });
+    }
+
     tracing::info!("User {} requests a match", body.user_id);
 
-    // TODO: better error handling
-    player_queue.write().unwrap().push_back(MatchingPlayer {
-        user_id: body.user_id,
-        tx: res_tx,
-    });
+    #[allow(unused_variables)]
+    let auto_drop = AutoDrop {
+        user_id: body.user_id.clone(),
+        queue: player_queue.clone(),
+    };
 
     notify_tx.send(()).await.unwrap();
 
-    match res_rx.await.unwrap() {
-        Some(res) => (StatusCode::OK, Json(res)),
-        None => (
-            StatusCode::INTERNAL_SERVER_ERROR,
-            Json(MatchResponse {
-                game_id: "".to_string(),
-                color: Color::White,
-            }),
-        ),
+    let res = res_rx.await.unwrap();
+    if res.is_err() {
+        (StatusCode::INTERNAL_SERVER_ERROR, Json(res))
+    } else {
+        (StatusCode::OK, Json(res))
     }
 }
 
@@ -142,7 +147,7 @@ async fn main() {
 
     let (notify_tx, notify_rx) = mpsc::channel(MAX_CHANNEL_SIZE);
 
-    let player_queue = Arc::new(RwLock::new(VecDeque::<MatchingPlayer>::new()));
+    let player_queue = Arc::new(Mutex::new(VecDeque::<MatchingPlayer>::new()));
 
     let app = Router::new().route(
         "/match",
