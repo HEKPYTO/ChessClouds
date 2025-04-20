1ca01183f2c0b6bb64f3ececf98e52e8167e409f
HEKPYTO <tsunyanapat@gmail.com>
2025-04-20 20:38:34 +0700

    PREP .env FILES
    EXTRACT ALL .env FIELDS

--- web/next.config.ts
+++ web/next.config.ts
@@ -2,18 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  async rewrites() {
-    return [
-      {
-        source: '/init',
-        destination: 'http://localhost:8000/init'
-      },
-      {
-        source: '/games',
-        destination: 'http://localhost:8000/games'
-      }
-    ]
-  }
 };
 
 export default nextConfig;
