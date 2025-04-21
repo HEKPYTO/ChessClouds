f7755a04fad30a591c4db7975587b6e6540cf0f1
HEKPYTO <tsunyanapat@gmail.com>
2025-04-21 22:13:57 +0700

    FIX DOCKERFILE

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
-  env: {
-    NEXT_PUBLIC_GOOGLE_AUTH_ENDPOINT: process.env.NEXT_PUBLIC_GOOGLE_AUTH_ENDPOINT,
-    NEXT_PUBLIC_ENGINE_API_URL: process.env.NEXT_PUBLIC_ENGINE_API_URL,
-    NEXT_PUBLIC_WS_SERVER_URL: process.env.NEXT_PUBLIC_WS_SERVER_URL,
-    NEXT_PUBLIC_MATCHMAKING_SERVER_URL: process.env.NEXT_PUBLIC_MATCHMAKING_SERVER_URL
-  }
+  output: 'standalone'
 };
 
 export default nextConfig;
